51549713acb6074bec454d43a064d57ca6578385
natdanai-sssssss <usergodbot02@gmail.com>
2024-07-31 10:40:30 +0700

    modified id-code

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -5,7 +5,15 @@
       @submit="onSubmit"
       @reset="onReset"
       class="q-gutter-md"
-    >
+    ><!-- id -->
+      <q-input
+        filled
+        v-model="id"
+        label="Your id *"
+        hint="id-code"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please type your id']"
+      />
       <!-- name -->
       <q-input
         filled
@@ -56,12 +64,14 @@ export default {
   setup () {
     const $q = useQuasar()
 
+    const id = ref("6604101327")
     const name = ref("Natdanai")
     const surname = ref("Tasak")
     const age = ref(null)
     const accept = ref(false)
 
     return {
+      id,
       name,
       surname,
       age,
